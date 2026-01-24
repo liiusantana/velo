5423cc2224aa17b862ec117959996ed8a62fe452
Livia Santana <liu_lsa@hotmail.com>
2026-01-23 21:30:06 -0300

    voltahndo para o titulo inicial

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('a Webapp deve estar online', async ({ page }) => {
+test('Webapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   // Expect a title "to contain" a substring.
