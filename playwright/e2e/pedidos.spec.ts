411e4a0b2060c60dba4a4c6cdb307f0f11ff2b1b
Livia Santana <liu_lsa@hotmail.com>
2026-01-26 20:32:31 -0300

    iniciando a automação do teste de consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+
+test('Deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+
+  //Checkpoint
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+
+   //Checkpoint
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByTestId('search-order-id').fill('VLO-KSP8V3')
+  await page.getByTestId('search-order-button').click()
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-KSP8V3')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+})
