247e3fa5d3f08fafb78ffa68e5d4ac78573aeb0f
Livia Santana <liu_lsa@hotmail.com>
2026-01-26 20:41:41 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test'
 
-test('Deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+// AAA - Arrange, Act, Assert - Padrão de teste do playwright (Preparação, Ação, Verificação)
 
+test('Deve consultar um pedido aprovado', async ({ page }) => {
+    //Arrange
+    await page.goto('http://localhost:5173/')
   //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
    //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-KSP8V3')
   await page.getByTestId('search-order-button').click()
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-KSP8V3')
 
